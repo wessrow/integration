c0ffa959b023435e55f8b30c9c34bfb8f54cfb83
Wessrow <gustav.larsson@acebit.se>
2024-11-30 20:51:27 +0000

    updated async_add_job to async_create_task - deprecation

--- custom_components/hasl3/__init__.py
+++ custom_components/hasl3/__init__.py
@@ -403,11 +403,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         return False
 
     try:
-        hass.async_add_job(
+        hass.async_create_task(
             hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-            
         )
-        hass.async_add_job(
+        hass.async_create_task(
             hass.config_entries.async_forward_entry_setups(entry, ["binary_sensor"])
         )
         logger.debug("[setup_entry] Forward entry setup succeeded")
@@ -439,10 +438,10 @@ async def async_unload_entry(hass, entry):
     logger.debug("[unload_entry] Entered")
 
     try:
-        hass.async_add_job(
+        hass.async_create_task(
             hass.config_entries.async_forward_entry_unload(entry, "sensor")
         )
-        hass.async_add_job(
+        hass.async_create_task(
             hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
         )
     except:
